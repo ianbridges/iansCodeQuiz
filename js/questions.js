c23a606775b64b36e7d3d573ecedd218a140988f
ianbridges <megabridge12@gmail.com>
2022-10-12 16:35:48 -0500

    questions added to questions.js

---
+++ js/questions.js
@@ -0,0 +1,30 @@
+var questions = [
+    {
+        text: "Commonly used data types DO NOT include?",
+        answers: ["1. strings" , "2. booleans" , "3. alerts" , "4. numbers"],
+        correctAnswer: "booleans"
+    },
+    {
+        text: "The condition in an if / else statement is enclosed with _______?",
+        answers: ["1. quotes" , "2. curly brackets" , "3. parenthesis" , "4. square brackets"],
+        correctAnswer: "parenthesis"
+    },
+    {
+        text: "Arrays in JavaScript can be used to store _______ ?",
+        answers: ["1. numbers and strings" , "2. other arrays" , "3. booleans" , "4. all of the above"],
+        correctAnswer: "all of the above"
+    },
+    {
+        text: " String values must be enclosed within _______ when being assigned a variable.",
+        answers: ["1. commas" , "2. curly brackets" , "3. quotes" , "4. parenthesis"],
+        correctAnswer: "curly brackets"
+    },
+   
+        {
+        text: "A very useful tool used during development and debugging for printing content to the debugger is?",
+        answers: ["1. JavaScript", "2. for loops", "3. console.log", "4. terminal/bash"],
+        correctAnswer: "for loops"
+    }
+];
+
+
