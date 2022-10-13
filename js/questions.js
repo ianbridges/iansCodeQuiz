aaa654656df3153a16a6b8c5eb6b123b4e433fd2
ianbridges <megabridge12@gmail.com>
2022-10-12 20:09:27 -0500

    questions testing added

--- js/questions.js
+++ js/questions.js
@@ -2,29 +2,27 @@ var questions = [
     {
         text: "Commonly used data types DO NOT include?",
         answers: ["1. strings" , "2. booleans" , "3. alerts" , "4. numbers"],
-        correctAnswer: "booleans"
+        correctAnswer: "2. booleans"
     },
     {
         text: "The condition in an if / else statement is enclosed with _______?",
         answers: ["1. quotes" , "2. curly brackets" , "3. parenthesis" , "4. square brackets"],
-        correctAnswer: "parenthesis"
+        correctAnswer: "3. parenthesis"
     },
     {
         text: "Arrays in JavaScript can be used to store _______ ?",
         answers: ["1. numbers and strings" , "2. other arrays" , "3. booleans" , "4. all of the above"],
-        correctAnswer: "all of the above"
+        correctAnswer: "4. all of the above"
     },
     {
         text: " String values must be enclosed within _______ when being assigned a variable.",
         answers: ["1. commas" , "2. curly brackets" , "3. quotes" , "4. parenthesis"],
-        correctAnswer: "curly brackets"
+        correctAnswer: "2. curly brackets"
     },
    
         {
         text: "A very useful tool used during development and debugging for printing content to the debugger is?",
         answers: ["1. JavaScript", "2. for loops", "3. console.log", "4. terminal/bash"],
-        correctAnswer: "for loops"
+        correctAnswer: "2. for loops"
     }
 ];
-
-
--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ var questionNumber = 0;
 var timer = 60;
 var interval;
 
+document.getElementById("quizPage").style.display = "none";
+
 function start() {
     score = 0;
     questionNumber = 0;
@@ -21,3 +23,37 @@ function updateTimer() {
         endQuiz();
     }
 }
+
+function nextQuestion() {
+    document.getElementById("question").innerHTML = questions[questionNumber].text;
+    document.getElementById("a").textContent = questions[questionNumber].answers[0];
+    document.getElementById("b").textContent = questions[questionNumber].answers[1];
+    document.getElementById("c").textContent = questions[questionNumber].answers[2];
+    document.getElementById("d").textContent = questions[questionNumber].answers[3];
+}
+
+function checkAnswer() {
+    var answer = this.textContent;
+    if (answer == questions[questionNumber].correctAnswer) {
+        document.getElementById("answer").innerHTML = "Correct!";
+        score++
+    }
+    else {
+        document.getElementById("answer").innerHTML = "Wrong!";
+        timer -= 10;
+    }
+    questionNumber++;
+    if (questionNumber < questions.length) {
+        nextQuestion();
+    }
+    else {
+        clearInterval(interval);
+        endQuiz();
+    }
+}
+
+document.getElementById("a").addEventListener("click", checkAnswer);
+document.getElementById("b").addEventListener("click", checkAnswer);
+document.getElementById("c").addEventListener("click", checkAnswer);
+document.getElementById("d").addEventListener("click", checkAnswer);
+document.getElementById("startButton").addEventListener("click", start);
